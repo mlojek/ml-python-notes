acb864d854331c50d6ad70333edb73ba9989e611
Marcin <mlojek@protonmail.com>
2022-05-18 20:52:01 +0200

    Add sequential_mnist - sequential neural network using keras tensorflow - network classifies handwritten characters from mnist set

---
+++ sequential_mnist.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# import handwritten digits dataset
+from tensorflow.keras.datasets import mnist
+
+# get the data:
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# reshape input data:
+# leave the first dimension unchanged, then flatten (28, 28) to (784):
+x_train = x_train.reshape(-1, 784)
+x_test = x_test.reshape(-1, 784)
+# optionally change f64 to f32 to ease computation:
+x_train = x_train.astype("float32")
+x_test = x_test.astype("float32")
+# normalize the values (is it necessary tho?):
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# make a sequential model:
+model = keras.Sequential(
+    [
+        keras.Input(shape=(784)),
+        layers.Dense(512, activation='relu'),
+        layers.Dense(256, activation='relu'),
+        layers.Dense(10),
+    ]
+)
+
+# specify other aspects of the model:
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(lr=0.001),
+    metrics=['accuracy'],
+)
+
+# get info about net structure:
+model.summary()
+
+# train the net:
+model.fit(x_train, y_train, batch_size=32, epochs=10, verbose=2)
+
+# evaluate the net:
+model.evaluate(x_test, y_test, batch_size=32, verbose=2)
